efc1d79082339dcf04fa9e4d3b03f7a630e5fb4d
Andrew Benson <abenson@gmail.com>
2017-10-03 09:22:42 -0500

    Add hand scoring.

--- PlayerHand.m
+++ PlayerHand.m
@@ -30,17 +30,100 @@ NSString *stringForPlayerHand(PlayerHand ph) {
 
 PlayerHand ScoreHand(id hand, int *highVal)
 {
-	id en, obj, sorted;
-	int val, bestHand;
+	id en, sorted;
+	int bestHand, c;
+	id count, counts;
+	Die *die;
+	NSNumber *number, *key;
+
+	counts = [[NSMutableDictionary alloc] init];
 
 	sorted = [hand sortedArrayUsingSelector: @selector(compare:)];
-	en = [sorted objectEnumerator];
 
 	bestHand = Nothing;
 
-	while( (obj = [en nextObject]))	{
+	en = [sorted objectEnumerator];
+
+	while((die = [en nextObject])) {
+		count = [counts objectForKey: [NSNumber numberWithInt: [die value]]];
+		if(count == nil) {
+			[counts setObject: [NSNumber numberWithInt: 1] forKey: [NSNumber numberWithInt: [die value]]];
+		} else {
+			[counts setObject: [NSNumber numberWithInt: ([count intValue]+1)] forKey: [NSNumber numberWithInt: [die value]]];
+		}
+	}
+
+	en = [counts keyEnumerator];
+
+
+	/* Find multiples */
+	while((key = [en nextObject])) {
+		number = [counts objectForKey: key];
+		if([number intValue] == 2) {
+			if(bestHand == Pair) {
+				bestHand = TwoPair;
+				if(*highVal < [key intValue]) {
+					*highVal = [key intValue];
+				}
+			} else if(bestHand == Set) {
+				bestHand = FullHouse;
+			} else {
+				bestHand = Pair;
+				*highVal = [key intValue];
+			}
+		}
+		if([number intValue] == 3) {
+			if(bestHand == Pair) {
+				bestHand = FullHouse;
+				*highVal = [key intValue];
+			} else {
+				bestHand = Set;
+				*highVal = [key intValue];
+			}
+		}
+		if([number intValue] == 4) {
+			bestHand = FourAKind;
+			*highVal = [key intValue];
+		}
+		if([number intValue] == 5) {
+			bestHand = FiveAKind;
+			*highVal = [key intValue];
+		}
+	}
+
+	/*Find straights*/
+	if(bestHand == Nothing) {
+		en = [sorted objectEnumerator];
+		die = [en nextObject];
+		c = [die value];
+		if(c < 2) {
+			while((die = [en nextObject])) {
+				if([die value] != ++c) {
+					break;
+				}
+			}
+			if(c == 5) {
+				bestHand = LowStraight;
+			} else if(c == 6) {
+				bestHand = HighStraight;
+			}
+		}
 
 	}
+
+	/* If all else, find the high card */
+	if(bestHand == Nothing) {
+		en = [counts keyEnumerator];
+		key = [en nextObject];
+		*highVal = [key intValue];
+		while((key = [en nextObject])) {
+			if(*highVal < [key intValue]) {
+				*highVal = [key intValue];
+			}
+		}
+	}
+
+	[counts release];
 
 	return bestHand;
 }
